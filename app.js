3875d5c7302b0c2764f0796f54d252eb5c9d5def
IAmAPersson <iamaperson620@gmail.com>
2020-04-20 22:21:27 -0500

    updating to use tabs instead of a mix of tabs and spaces

--- app.js
+++ app.js
@@ -21,30 +21,31 @@ client.on("guildDelete", guild =>
 
 client.on("message", async message =>
 {
-    console.log(message.author + ": " + message.content);
-    if (message.author.bot) return;
+	console.log(message.author + ": " + message.content);
+	if (message.author.bot) return;
 
-    if (message.content === "<@!359138435203596291>" || message.content === "<@359138435203596291>") {
-        message.channel.send("What's up " + message.author);
-    }
+	if (message.content === "<@!359138435203596291>" || message.content === "<@359138435203596291>")
+		message.channel.send("What's up " + message.author);
 
 	if (message.content.substring(0, 10) === "!nametheme")
 	{
 		const command = message.content.split(" ");
 		if (command[1] == "set")
-        {
-            fs.writeFileSync("nametheme.txt", command.splice(2).join(" "));
-            let sendmessage = "Name theme set to: " + fs.readFileSync("nametheme.txt");
-            message.channel.send(sendmessage);
-			
+		{
+			fs.writeFileSync("nametheme.txt", command.splice(2).join(" "));
+			let sendmessage = "Name theme set to: " + fs.readFileSync("nametheme.txt");
+			message.channel.send(sendmessage);
+
 			setTimeout(() =>
 			{
 				client.channels.get("525945106193448970").send(sendmessage);
 			}, 5000);
-        }
-        else
-            message.channel.send("Current theme: " + fs.readFileSync("nametheme.txt"));
+		}
+		else
+			message.channel.send("Current theme: " + fs.readFileSync("nametheme.txt"));
 	}
+	else if (message.content.substring(0, 5) === "!ping")
+		message.channel.send("Pong!");
 });
 
 client.login(config.token);
